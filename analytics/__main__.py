1c24de8affa26e1eabc8e5cbc8d9557042f369c0
kevin-mahon <k.mahon.dev@gmail.com>
2025-09-30 22:12:24 -0400

    updated api key

--- analytics/__main__.py
+++ analytics/__main__.py
@@ -4,7 +4,7 @@ from .analengine import run_analysis
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run data analytics on a specified dataset.")
-    parser.add_argument("--api_key", type=str, help="Riot Games API key.", default="RGAPI-3074b506-5569-4d50-940c-099233ac3560")
+    parser.add_argument("--api_key", type=str, help="Riot Games API key.", default="RGAPI-f98c9c45-17e7-464e-bb59-82cb1da5e837")
     parser.add_argument("--summoner", type=str, required=True, help="Gamertag/Summoner name to analyze matches of.")
     parser.add_argument("--tag", type=str, required=True, help="Tag of summoner.")
     args = parser.parse_args()
