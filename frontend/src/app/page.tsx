fd03afba4272f95fbbddd22de4363f89f4ce6d0f
jaredamir <jaredamir@yahoo.com>
2025-10-03 04:19:22 -0400

    added input field component and updated animated hexcore component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,43 @@
 "use client";
+import { useState, useEffect } from "react";
+
 import Hexcore from "./components/hexcore";
 import AnimatedHexcore from "./components/animatedHexcore";
+import HexInput from "./components/hexInput";
+
 
 const Home: React.FC = () => {
+  const DEFAULT_COLOR = "#131474";
+  const PRESSED_COLOR = "#74136A";
+
+  const [gradientColor, setGradientColor] = useState(DEFAULT_COLOR);
+  const [inputError, setInputError] = useState(false);
   
+
   return (
-    <main>
-      <Hexcore color="white" width={200} height={200} />
-      <AnimatedHexcore />
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        background: `radial-gradient(circle, ${gradientColor} 0%, black 100%)`,
+        transition: "any 0.2s ease",
+      }}
+    >
+      <AnimatedHexcore color="#89EFFF"/>
+      <HexInput
+        width={"250px"}
+        borderColor="white"
+        glowColor={inputError ? "red": "#44A8FF"}
+        label="Enter Your League ID"
+        fontSize={"25px"}
+        placeholder = "e.g. 17822832"
+      />
     </main>
-  )
+  );
 };
 
 export default Home;
