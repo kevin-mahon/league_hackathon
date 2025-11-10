1c5c3ed9bb6716bea1f6acc79696f88fd52d34fa
kevin-mahon <k.mahon.dev@gmail.com>
2025-11-09 23:28:25 -0500

    updated my api key

--- analytics/__main__.py
+++ analytics/__main__.py
@@ -5,10 +5,11 @@ from .analyzer import run_analysis
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run data analytics on a specified dataset.")
-    parser.add_argument("--api_key", type=str, help="Riot Games API key.", default="RGAPI-f98c9c45-17e7-464e-bb59-82cb1da5e837")
+    parser.add_argument("--api_key", type=str, help="Riot Games API key.", default="RGAPI-114fc8d1-e4b3-4bde-a194-89f248ee08c1")
     parser.add_argument("--summoner", type=str, required=True, help="Gamertag/Summoner name to analyze matches of.")
     parser.add_argument("--tag", type=str, required=True, help="Tag of summoner.")
     args = parser.parse_args()
 
+    print(args.api_key)
     analysis = run_analysis(args.summoner, args.tag, "na1", args.api_key)
     print(analysis)
